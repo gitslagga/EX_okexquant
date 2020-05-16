6d9327b994e699d9faffc0ee8397107c4a0e99a7
slagga <957766762@qq.com>
2020-05-16 18:10:05 +0800

    add apis

--- db/db_mongo.go
+++ db/db_mongo.go
@@ -3,23 +3,24 @@ package db
 import (
 	"EX_okexquant/config"
 	"EX_okexquant/mylog"
+	"EX_okexquant/trade"
 	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"time"
 )
 
 var (
-	err           error
-	client        *mongo.Client
-	collection    *mongo.Collection
-	insertOneRes  *mongo.InsertOneResult
-	insertManyRes *mongo.InsertManyResult
-	deleteRes     *mongo.DeleteResult
-	updateRes     *mongo.UpdateResult
-	cursor        *mongo.Cursor
-	size          int64
+	err          error
+	client       *mongo.Client
+	collection   *mongo.Collection
+	insertOneRes *mongo.InsertOneResult
+	deleteRes    *mongo.DeleteResult
+	updateRes    *mongo.UpdateResult
+	cursor       *mongo.Cursor
+	size         int64
 )
 
 func InitMongoCli() {
@@ -51,4 +52,111 @@ func getContext() context.Context {
 	defer cancel()
 
 	return ctx
+}
+
+func GetFuturesInstrumentsTicker() (interface{}, error) {
+	collection = client.Database("main_quantify").Collection("futures_instruments_ticker")
+	cursor, err = collection.Find(getContext(), bson.D{})
+	if err == mongo.ErrNoDocuments {
+		list, err := trade.OKexClient.GetAccountCurrencies()
+		if err != nil {
+			mylog.Logger.Fatal().Msgf("[GetFuturesInstrumentsTicker] OKexClient GetAccountCurrencies failed, err=%v, list=%v", err, list)
+			return nil, err
+		}
+
+		var dataArray []interface{}
+		for i := 0; i < len(*list); i++ {
+			dataArray = append(dataArray, (*list)[i])
+		}
+
+		_, _ = collection.InsertMany(getContext(), dataArray)
+		return dataArray, nil
+	}
+
+	if err != nil {
+		mylog.Logger.Fatal().Msgf("[GetFuturesInstrumentsTicker] collection Find failed, err=%v, cursor=%v", err, cursor)
+		return nil, err
+	}
+	defer cursor.Close(context.Background())
+
+	var record map[string]string
+	var dataArray []map[string]string
+	for cursor.Next(context.Background()) {
+		_ = cursor.Decode(&record)
+		dataArray = append(dataArray, record)
+	}
+
+	return dataArray, nil
+}
+
+func ISExistsInstrumentsTicker(instrumentID string) bool {
+	collection = client.Database("main_quantify").Collection("futures_instruments_ticker")
+	size, err = collection.CountDocuments(getContext(), bson.D{{"instrument_id", instrumentID}})
+
+	if err != nil {
+		mylog.Logger.Fatal().Msgf("[ISExistsInstrumentsTicker] collection CountDocuments failed, err=%v, collection=%v", err, collection)
+		return false
+	}
+
+	if size <= 0 {
+		return false
+	}
+
+	return true
+}
+
+func GetFuturesInstrumentPosition(instrumentID string) (interface{}, error) {
+	position, err := trade.OKexClient.GetFuturesInstrumentPosition(instrumentID)
+	if err != nil {
+		mylog.Logger.Fatal().Msgf("[GetFuturesInstrumentsPosition] trade OKexClient failed, err=%v, position=%v", err, position)
+		return nil, err
+	}
+
+	collection = client.Database("main_quantify").Collection("futures_instruments_position")
+	size, err = collection.CountDocuments(getContext(), bson.D{})
+	if err != nil {
+		mylog.Logger.Fatal().Msgf("[InsertFuturesInstrumentsTicker] collection CountDocuments failed, err=%v, collection=%v", err, collection)
+		return nil, err
+	}
+
+	if size <= 0 {
+		_, _ = collection.InsertOne(getContext(), *position)
+	}
+	_, _ = collection.UpdateOne(getContext(), bson.D{{"instrument_id", instrumentID}}, *position)
+
+	return *position, nil
+}
+
+func GetFuturesUnderlyingLeverage(underlying string) (interface{}, error) {
+	leverage, err := trade.OKexClient.GetFuturesAccountsLeverage(underlying)
+	if err != nil {
+		mylog.Logger.Fatal().Msgf("[GetFuturesInstrumentsPosition] trade OKexClient failed, err=%v, leverage=%v", err, leverage)
+		return nil, err
+	}
+
+	collection = client.Database("main_quantify").Collection("futures_instruments_leverage")
+	size, err = collection.CountDocuments(getContext(), bson.D{})
+	if size <= 0 {
+		_, _ = collection.InsertOne(getContext(), leverage)
+	}
+	_, _ = collection.UpdateOne(getContext(), bson.D{{"underlying", underlying}}, leverage)
+
+	return leverage, nil
+}
+
+func SetFuturesUnderlyingLeverage(underlying, leverage string) (interface{}, error) {
+	resp, err := trade.OKexClient.PostFuturesAccountsLeverage(underlying, leverage, nil)
+	if err != nil {
+		mylog.Logger.Fatal().Msgf("[SetFuturesUnderlyingLeverage] trade OKexClient failed, err=%v, resp=%v", err, resp)
+		return nil, err
+	}
+
+	collection = client.Database("main_quantify").Collection("futures_instruments_leverage")
+	size, err = collection.CountDocuments(getContext(), bson.D{})
+	if size <= 0 {
+		_, _ = collection.InsertOne(getContext(), resp)
+	}
+	_, _ = collection.UpdateOne(getContext(), bson.D{{"underlying", underlying}}, resp)
+
+	return resp, nil
 }
--- tasks/task_service.go
+++ tasks/task_service.go
@@ -1,28 +1,135 @@
 package tasks
 
 import (
+	"EX_okexquant/data"
+	"EX_okexquant/db"
+	"EX_okexquant/mylog"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func InitRouter(r *gin.Engine) {
-	//r.POST("/quant/get_currency_list", GetCurrencyList)
+	/****************************** 交割合约 *********************************/
+	r.POST("/api/futures/instruments/ticker", GetFuturesInstrumentsTicker)
+	r.POST("/api/futures/position/:instrument_id", GetFuturesInstrumentPosition)
+	r.POST("/api/futures/get/leverage/:underlying", GetFuturesUnderlyingLeverage)
+	r.POST("/api/futures/set/leverage/:underlying/:leverage", SetFuturesUnderlyingLeverage)
 }
 
-//func GetCurrencyList(c *gin.Context) {
-//	out := data.CommonResp{}
-//
-//	list, err := trade.GetAccountCurrencies()
-//	if err != nil {
-//		out.ErrorCode = data.EC_NETWORK_ERR
-//		out.ErrorMessage = data.ErrorCodeMessage(data.EC_NETWORK_ERR)
-//		c.JSON(http.StatusBadRequest, out)
-//		return
-//	}
-//
-//	out.ErrorCode = data.EC_NONE.Code()
-//	out.ErrorMessage = data.EC_NONE.String()
-//	out.Data = list
-//
-//	c.JSON(http.StatusOK, out)
-//	return
-//}
+/**
+获取交割合约全部交易对
+*/
+func GetFuturesInstrumentsTicker(c *gin.Context) {
+	out := data.CommonResp{}
+
+	list, err := db.GetFuturesInstrumentsTicker()
+	if err != nil {
+		out.ErrorCode = data.EC_NETWORK_ERR
+		out.ErrorMessage = data.ErrorCodeMessage(data.EC_NETWORK_ERR)
+		c.JSON(http.StatusBadRequest, out)
+		return
+	}
+
+	out.ErrorCode = data.EC_NONE.Code()
+	out.ErrorMessage = data.EC_NONE.String()
+	out.Data = list
+
+	c.JSON(http.StatusOK, out)
+	return
+}
+
+/**
+获取交割合约单个合约持仓信息
+*/
+func GetFuturesInstrumentPosition(c *gin.Context) {
+	out := data.CommonResp{}
+	instrumentID := c.Param("instrument_id")
+	mylog.Logger.Info().Msgf("[Task Service] GetFuturesPosition request param: %s", instrumentID)
+
+	if !db.ISExistsInstrumentsTicker(instrumentID) {
+		out.ErrorCode = data.EC_PARAMS_ERR
+		out.ErrorMessage = data.ErrorCodeMessage(data.EC_PARAMS_ERR)
+		c.JSON(http.StatusBadRequest, out)
+		return
+	}
+
+	list, err := db.GetFuturesInstrumentPosition(instrumentID)
+	if err != nil {
+		out.ErrorCode = data.EC_NETWORK_ERR
+		out.ErrorMessage = data.ErrorCodeMessage(data.EC_NETWORK_ERR)
+		c.JSON(http.StatusBadRequest, out)
+		return
+	}
+
+	out.ErrorCode = data.EC_NONE.Code()
+	out.ErrorMessage = data.EC_NONE.String()
+	out.Data = list
+
+	c.JSON(http.StatusOK, out)
+	return
+}
+
+/**
+获取交割合约杠杆倍数
+*/
+func GetFuturesUnderlyingLeverage(c *gin.Context) {
+	out := data.CommonResp{}
+
+	underlying := c.Param("underlying")
+	mylog.Logger.Info().Msgf("[Task Service] GetFuturesUnderlyingLeverage request param: %s", underlying)
+
+	if underlying == "" {
+		out.ErrorCode = data.EC_PARAMS_ERR
+		out.ErrorMessage = data.ErrorCodeMessage(data.EC_PARAMS_ERR)
+		c.JSON(http.StatusBadRequest, out)
+		return
+	}
+
+	list, err := db.GetFuturesUnderlyingLeverage(underlying)
+	if err != nil {
+		out.ErrorCode = data.EC_NETWORK_ERR
+		out.ErrorMessage = data.ErrorCodeMessage(data.EC_NETWORK_ERR)
+		c.JSON(http.StatusBadRequest, out)
+		return
+	}
+
+	out.ErrorCode = data.EC_NONE.Code()
+	out.ErrorMessage = data.EC_NONE.String()
+	out.Data = list
+
+	c.JSON(http.StatusOK, out)
+	return
+}
+
+/**
+设置交割合约杠杆倍数
+*/
+func SetFuturesUnderlyingLeverage(c *gin.Context) {
+	out := data.CommonResp{}
+
+	underlying := c.Param("underlying")
+	leverage := c.Param("leverage")
+	mylog.Logger.Info().Msgf("[Task Service] SetFuturesUnderlyingLeverage request param: %s, %s", underlying, leverage)
+
+	if underlying == "" || leverage == "" {
+		out.ErrorCode = data.EC_PARAMS_ERR
+		out.ErrorMessage = data.ErrorCodeMessage(data.EC_PARAMS_ERR)
+		c.JSON(http.StatusBadRequest, out)
+		return
+	}
+
+	list, err := db.SetFuturesUnderlyingLeverage(underlying, leverage)
+	if err != nil {
+		out.ErrorCode = data.EC_NETWORK_ERR
+		out.ErrorMessage = data.ErrorCodeMessage(data.EC_NETWORK_ERR)
+		c.JSON(http.StatusBadRequest, out)
+		return
+	}
+
+	out.ErrorCode = data.EC_NONE.Code()
+	out.ErrorMessage = data.EC_NONE.String()
+	out.Data = list
+
+	c.JSON(http.StatusOK, out)
+	return
+}
